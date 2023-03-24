fdf9002357d5040e018be94985ec93d19dd56f95
Cedric Rabarijohn <cedrictianarabarijohn@gmail.com>
2023-03-25 00:16:29 +0300

    adding env

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ import random
 from flask import Flask, request
 from pymessenger.bot import Bot
 import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 # This is page access token that you get from facebook developer console.
-PAGE_ACCESS_TOKEN = 'EAAQnS5EXIrkBALKFavsrWZAChkeI91B6jDG4VJGzqZAQbcTN6uu3O2Hi2ivF0Lq78fgTfzaZAaGQDnbe6bdP1bZA1QOOBYLmcsh52zYZCAEjpb3TbPa1sDLLe4RTvdk8ffl8TnI9oV7MjQyoSinakLn6ovfqgukZClrWQyXybB5z8IP6Eq0NNc'
+PAGE_ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+print(PAGE_ACCESS_TOKEN)
 VERIFY_TOKEN = 'athackhiu'
 # This is API key for facebook messenger.
 API = "https://graph.facebook.com/LATEST-API-VERSION/me/messages?access_token="+PAGE_ACCESS_TOKEN
